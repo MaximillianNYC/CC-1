f69c362290c8fe5d99116067a2337a0af753fd08
MaximillianNYC <max@mxmlln.com>
2024-08-21 11:08:48 -0400

    updates to prompts v1.0 but results still subpar

--- src/API.js
+++ src/API.js
@@ -15,7 +15,10 @@ const API = {
 
   getAISolution: async (equation) => {
     try {
-      const prompt = `Solve this conceptual equation: ${equation}. Respond in a concept that can be in explained in less than 5 words and is preceded by an emoji that best illustrates the concept. Match the case format of the input text. Avoid uncreative combinations of the input words, instead be creative in your answer. Answer should be entertaining, compelling & calculated. Make sure it is a raw, unfiltered take that cuts out the softer, diplomatic phrasing and leaning into the reality of the situation, even if it’s blunt.`;
+      const prompt = 
+        `
+          Solve this conceptual equation: ${equation}. 
+        `;
       const response = await axios.post(`${API_BASE_URL}/concept-calculator`, { messages: [{ role: "user", content: prompt }] });
       
       if (response.data && response.data.choices && response.data.choices[0] && response.data.choices[0].message) {
